163d216d555c999b98e4166d28124f5174c78ed3
Kevin LeClair <adknowledgeuser@c02q82l7g8wm.adknowledge.com>
2017-09-12 16:08:45 -0400

    fixing animation duration and adding in baseball 3d model

--- src/BaseballScene/PitchAnimation.js
+++ src/BaseballScene/PitchAnimation.js
@@ -10,9 +10,10 @@ import createContainer from '../containers/GenericContainer.js';
 
 const PitchAnimation = (props) => {
   return (
-    <a-box color="#ff0000" width="0.1" height="0.3" depth="0.1"
-      alongpath={`curve: #pitch_${props.curveId}; loop:true; dur:${Number(props.animationTime) * 10}; rotate:true;`}>
-    </a-box>
+    <a-entity
+      obj-model="obj: #baseball-obj; mtl: #baseball-mtl"
+      alongpath={`curve: #pitch_${props.curveId}; loop:true; dur:${(Number(props.animationTime) * 10) * 1000}; rotate:true;`}>
+    </a-entity>
   );
 };
 
